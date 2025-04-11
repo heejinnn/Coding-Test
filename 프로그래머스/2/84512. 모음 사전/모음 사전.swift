c8fa19ae0e11bc20b1213c189b2ffe25f5ac7dcd
최희진 <103185302+heejinnn@users.noreply.github.com>
2025-04-11 23:30:32 +0900

    [level 2] Title: 모음 사전, Time: 1.21 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.swift
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.swift
@@ -1,25 +1,19 @@
 import Foundation
 
- 
 func solution(_ word:String) -> Int {
-    let word = word.map { String($0) }
-    let dict = ["A": 1, "E": 2, "I": 3, "O": 4, "U": 5]
-    var nums = [1]
-    var sum = 0
+    let alpha = "AEIOU".map { String($0) }
+    var result = [String]()
 
-    for _ in 1..<5 {
-        if let num = nums.last {
-            nums.append(num * 5 + 1)
+    func dfs(_ str: String) {
+        result.append(str)
+
+        if str.count == 5 { return }
+
+        for i in 0..<5 {
+            dfs(str + alpha[i])
         }
     }
 
-    nums.reverse()
-
-    for i in 0..<word.count {
-        if let n = dict[word[i]] {
-            sum += nums[i] * (n-1) + 1
-        }
-    }
-
-    return sum
+    dfs("")
+    return result.firstIndex(of: word)!
 }
