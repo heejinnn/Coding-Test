315f18fe629c420194aab8bc49065b6f12058510
최희진 <103185302+heejinnn@users.noreply.github.com>
2025-10-10 18:43:38 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 730.02 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.swift
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.swift
@@ -2,27 +2,31 @@ import Foundation
 
 func solution(_ users:[[Int]], _ emoticons:[Int]) -> [Int] {
     
-    // 1. 할 수 있는 모든 할인율 조합 찾기
-    // 1-1. 중복 순열
-    // 2. 각 할인율 조합에 따른 user의 가입 여부
-    // 2-1. 할인율이 최소 할인율보다 크거나 같으면 → 구매, 합 계산
-    // 2-2. 할인율이 최소 할인율보다 작으면 -> 패스
-    // 3. 가입자 수가 가장 많고, 같으면 수익이 높은 조합 저장
+    // 1. 가능한 모든 할인율 조합을 완전탐색 (DFS)
+    // 2. 깊이가 이모티콘 개수에 도달하면, 현재 할인 조합을 기준으로 사용자별 계산 수행
+    // 3. 각 사용자에 대해
+    // 3-1. 최소 할인 비율과 구매 기준 금액을 확인
+    // 3-2. 모든 이모티콘 탐색하며, 할인율이 사용자 기준 이상이면 구매 금액에 반영
+    // 3-3. 최종 구매 금액이 기준 금액 이상이면 => 가입자 수 증가, 기준 금액 미만이면 => 판매액에 누적
+    // 4. 모든 사용자 계산이 끝난 뒤
+    // => 현재 조합 결과를 기존 result와 비교
+    // => 가입자 수가 더 많거나, 같다면 판매액이 더 높은 경우 result 갱신
+    // 5. 아직 모든 이모티콘에 대해 할인율을 정하지 않았다면, [10, 20, 30, 40] 중 하나를 선택하고 다음 depth로 재귀 호출
+    // 6. DFS 종료 후, 최적의 [가입자 수, 판매액] 조합 반환
+
     
-    
-    let discounts = [10, 20, 30, 40]
-    var result = [0, 0] // [가입자 수, 판매액]
+    var result: [Int] = [0,0]
+    let discounts = [10,20,30,40]
     let emoticonCount = emoticons.count
-    
     var discountCombination: [Int] = Array(repeating: 0, count: emoticonCount)
     
-    func dfs(_ depth: Int) {
+    func dfs(_ depth: Int){
+        
         if depth == emoticonCount {
-            var joinCount = 0
-            var totalSales = 0
+            var joinCount = 0 
+            var totalPrice = 0
             
-            // 사용자마다 구매 확인
-            for user in users {
+            for user in users{
                 let userMinDiscount = user[0]
                 let userPriceLimit = user[1]
                 var userTotal = 0
@@ -38,13 +42,14 @@ func solution(_ users:[[Int]], _ emoticons:[Int]) -> [Int] {
                 if userTotal >= userPriceLimit {
                     joinCount += 1
                 } else {
-                    totalSales += userTotal
+                    totalPrice += userTotal
                 }
             }
             
-            if joinCount > result[0] || (joinCount == result[0] && totalSales > result[1]) {
-                result = [joinCount, totalSales]
+            if joinCount > result[0] || (joinCount == result[0] && totalPrice > result[1]) {
+                result = [joinCount, totalPrice]
             }
+            
             return
         }
         
@@ -53,7 +58,6 @@ func solution(_ users:[[Int]], _ emoticons:[Int]) -> [Int] {
             dfs(depth + 1)
         }
     }
-    
     dfs(0)
     
     return result
